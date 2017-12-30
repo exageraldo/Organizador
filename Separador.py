46f7367c5021acc988711a80cd67c76a317734a4
MaxPlay-BR <kawandersonf@gmail.com>
2017-12-30 13:35:05 -0200

    Add files via upload
    Primeiro teste :)

---
+++ Separador.py
@@ -0,0 +1,26 @@
+#Separador_De_Arquivos
+
+import shutil
+import glob
+import time
+import os
+
+def getMusicas():
+    i = 0
+    
+    mv = glob.glob1(r'C:\Users\ADELÂNDIA\Downloads\Telegram Desktop','*.MP3')
+    lc = r'C:\Users\ADELÂNDIA\Downloads\Telegram Desktop\Musicas'
+
+    try:
+        os.mkdir(r'C:\Users\ADELÂNDIA\Downloads\Telegram Desktop\Musicas')
+    except OSError:
+        print('A pasta Musicas ja foi criada!')
+    
+    while(i<= len(mv)):
+        shutil.move(mv[i],lc)
+        print(mv[i])
+        time.sleep(1)
+        i = i + 1
+
+getMusicas()
+
