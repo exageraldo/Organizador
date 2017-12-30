778cf821f218c1480cc5d7a047deb5e9a03cc827
MaxPlay-BR <kawandersonf@gmail.com>
2017-12-30 13:37:36 -0200

    Update Separador.py

--- Separador.py
+++ Separador.py
@@ -8,11 +8,11 @@ import os
 def getMusicas():
     i = 0
     
-    mv = glob.glob1(r'C:\Users\ADELÂNDIA\Downloads\Telegram Desktop','*.MP3')
-    lc = r'C:\Users\ADELÂNDIA\Downloads\Telegram Desktop\Musicas'
+    mv = glob.glob1(r'C:\Users\SEU_USER\Downloads\Telegram Desktop','*.MP3')
+    lc = r'C:\Users\SEU_USER\Downloads\Telegram Desktop\Musicas'
 
     try:
-        os.mkdir(r'C:\Users\ADELÂNDIA\Downloads\Telegram Desktop\Musicas')
+        os.mkdir(r'C:\Users\SEU_USER\Downloads\Telegram Desktop\Musicas')
     except OSError:
         print('A pasta Musicas ja foi criada!')
     
